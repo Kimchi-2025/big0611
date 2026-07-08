e15758900dac68c8af3c521c6fc5b131e549f07e
Kimchi-2025 <jjongbaragi@gmail.com>
2026-07-08 17:51:04 +0900

    수업_0708

---
+++ py/p02_데이터분석및시각화/ch03_주가데이터탐색실습/test.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import yfinance as yf
+
+# raw = yf.download("AAPL", start="2026-01-01", end="2026-06-30")
+raw = yf.download("NVDA", period="1y", interval="1d", multi_level_index=False)
+
+df = raw.copy()
+print(df['Close'].agg(['max', 'min', 'mean']).map('{:.0f}'.format))
+
+print(df['Volume'].agg(['mean', 'median', 'std']).map('{:,.0f}'.format))
+
+new = df[['Close', 'Volume']]
+
+print("tail", df.tail(10))
+
+print("iloc", df.iloc[-10:])
+
+print(len(df[df['Close'] > df['Close'].quantile(0.9)].index))
+print(df[df['Close'] > df['Close'].quantile(0.9)].index)
+
+
