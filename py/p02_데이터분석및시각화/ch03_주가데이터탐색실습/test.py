6b7fed8ddbde5df67728272a247934a8f7d4a1b8
Kimchi-2025 <jjongbaragi@gmail.com>
2026-07-10 17:42:43 +0900

    수업_0710

--- py/p02_데이터분석및시각화/ch03_주가데이터탐색실습/test.py
+++ py/p02_데이터분석및시각화/ch03_주가데이터탐색실습/test.py
@@ -7,18 +7,23 @@ import yfinance as yf
 # raw = yf.download("AAPL", start="2026-01-01", end="2026-06-30")
 raw = yf.download("NVDA", period="1y", interval="1d", multi_level_index=False)
 
-df = raw.copy()
-print(df['Close'].agg(['max', 'min', 'mean']).map('{:.0f}'.format))
+# df = raw.copy()
+# print(df['Close'].agg(['max', 'min', 'mean']).map('{:.0f}'.format))
 
-print(df['Volume'].agg(['mean', 'median', 'std']).map('{:,.0f}'.format))
+# print(df['Volume'].agg(['mean', 'median', 'std']).map('{:,.0f}'.format))
 
-new = df[['Close', 'Volume']]
+# new = df[['Close', 'Volume']]
 
-print("tail", df.tail(10))
+# print("tail", df.tail(10))
 
-print("iloc", df.iloc[-10:])
+# print("iloc", df.iloc[-10:])
 
-print(len(df[df['Close'] > df['Close'].quantile(0.9)].index))
-print(df[df['Close'] > df['Close'].quantile(0.9)].index)
+# print(len(df[df['Close'] > df['Close'].quantile(0.9)].index))
+# print(df[df['Close'] > df['Close'].quantile(0.9)].index)
 
-
+plt.figure(figsize=(10,5))
+plt.subplot(1,2,1)
+sns.histplot(raw, x='Close')
+plt.subplot(1,2,2)
+sns.histplot(raw, x='Close')
+plt.show()
